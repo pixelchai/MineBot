be577e646873a5e6469d74b4ccebcd8f39e8856f
pixelzerg <pixelzerg@gmail.com>
2020-07-03 20:09:18 +0100

    add Forge MDK

--- setup.bat
+++
@@ -1,7 +0,0 @@
-:: Deps: Java8 JDK (AdoptOpenJDK), python3, git
-:: NB: might need to restart PC after installing Java8 JDK
-pip3 install gym lxml numpy pillow
-git clone "https://github.com/Microsoft/malmo.git"
-cd malmo/Minecraft
-set /p VER=<../VERSION
-echo malmomod.version=%VER% > ./src/main/resources/version.properties
