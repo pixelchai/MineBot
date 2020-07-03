58a6a152abeafa0d034d12aedc7c16f2632153c7
pixelzerg <pixelzerg@gmail.com>
2020-07-03 20:53:50 +0100

    mod setup (metadata, etc)

--- src/main/java/com/pixelzerg/minebot/MineBot.java
+++ src/main/java/com/pixelzerg/minebot/MineBot.java
@@ -1,8 +1,14 @@
-package com.example.examplemod;
+package com.pixelzerg.minebot;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -19,13 +25,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("minebot")
+public class MineBot
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public MineBot() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -81,4 +87,35 @@ public class ExampleMod
             LOGGER.info("HELLO from Register Block");
         }
     }
+
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent event)
+    {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        Minecraft mc = Minecraft.getInstance();
+        String reply = null;
+
+        if(player != null) {
+            String msg = event.getMessage().getString();
+            if (msg.contentEquals("<"+ player.getDisplayName().getString() +"> start"))
+            {
+                reply = "How about no :)";
+//                KeyBindingUtils.setPressed(mc.gameSettings.keyBindForward, true);
+                KeyBinding.setKeyBindState(mc.gameSettings.keyBindForward.getKey(), true);
+            } else {
+                LOGGER.debug("<"+ player.getDisplayName().getString() +"> start");
+                LOGGER.debug(msg);
+            }
+
+            if (reply != null){
+                player.sendMessage(new StringTextComponent(reply));
+                event.setCanceled(true);
+            }
+        }
+    }
+
+//    @SubscribeEvent
+//    public void CommandEvent(CommandEvent event){
+//        event.
+//    }
 }
