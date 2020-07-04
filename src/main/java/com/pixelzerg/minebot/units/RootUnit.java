d6f52fc7ca2c1799e37681eb97cf30c79e894295
pixelzerg <pixelzerg@gmail.com>
2020-07-04 22:37:24 +0100

    selectors idea

--- src/main/java/com/pixelzerg/minebot/units/RootUnit.java
+++ src/main/java/com/pixelzerg/minebot/units/RootUnit.java
@@ -10,8 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class RootUnit extends Unit {
-    private static final Logger LOGGER = LogManager.getLogger();
-
     @SubscribeEvent
     public void onChat(ClientChatReceivedEvent event) {
         ClientPlayerEntity player = Minecraft.getInstance().player;
@@ -20,20 +18,7 @@ public class RootUnit extends Unit {
         if (player != null) {
             String msg = event.getMessage().getString();
             if (msg.contentEquals("<" + player.getDisplayName().getString() + "> start")) {
-                new MoveToUnit(player.getPosition().add(new Vec3i(10, 0, 0)))
-                    .on(Unit.EVENT_STARTED, new Runnable() {
-                        @Override
-                        public void run() {
-                            LOGGER.debug("Started MoveToUnit!");
-                        }
-                    })
-                    .on(Unit.EVENT_DONE, new Runnable() {
-                        @Override
-                        public void run() {
-                            LOGGER.debug("Finished MoveToUnit!");
-                        }
-                    })
-                    .start();
+                new MoveToUnit(player.getPosition().add(new Vec3i(10, 0, 0))).start();
             }
         }
     }
--- src/main/java/com/pixelzerg/minebot/units/Unit.java
+++ src/main/java/com/pixelzerg/minebot/units/Unit.java
@@ -1,11 +1,15 @@
 package com.pixelzerg.minebot.units;
 
 import net.minecraftforge.common.MinecraftForge;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Unit {
+    protected static final Logger LOGGER = LogManager.getLogger();
+
     public static final int EVENT_DONE = 0;
     public static final int EVENT_STARTED = 1;
 
@@ -25,6 +29,14 @@ public class Unit {
     }
 
     public void start(){
+        this.fireEvent(EVENT_STARTED);
         MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.debug("Unit: Started "+this.getClass().getSimpleName());
+    }
+
+    private void done(){
+        MinecraftForge.EVENT_BUS.unregister(this);
+        this.fireEvent(EVENT_DONE);
+        LOGGER.debug("Unit: Done "+this.getClass().getName());
     }
 }
